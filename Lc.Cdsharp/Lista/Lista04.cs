4edc761b696f96eb13e6b0414f96d82858fd8adc
teduardo <tiago.eduardo@capgemini.com>
2020-02-17 17:04:34 -0300

    Add lista4.

--- Lc.Cdsharp/Lista/Lista04.cs
+++ Lc.Cdsharp/Lista/Lista04.cs
@@ -7,7 +7,7 @@ namespace Lc.Csharp.Modulo1.Praticando
 {
     public class Lista04
     {
-        static ContaBancaria[] contas = new ContaBancaria[5];
+        static Conta[] contas = new Conta[3];
 
         static void Main()
         {
@@ -24,8 +24,9 @@ namespace Lc.Csharp.Modulo1.Praticando
             {
                 Console.WriteLine("\n## Escolhe uma opçao ##");
                 StringBuilder menu = new StringBuilder();
-                menu.Append("1 - Criar conta");
-                menu.Append("\n2 - Imprimir conta");
+                menu.Append("1 - Criar conta max");
+                menu.Append("\n2 - Criar conta universitaria");
+                menu.Append("\n3 - Imprimir conta");
                 menu.Append("\n0 - Sair");
                 Console.WriteLine(menu);
                 int tecla = int.Parse(Console.ReadLine());
@@ -33,9 +34,12 @@ namespace Lc.Csharp.Modulo1.Praticando
                 switch (tecla)
                 {
                     case 1:
-                        AdicionarConta();
+                        AdicionarContaMax();
                         break;
                     case 2:
+                        AdicionarContaUniversitaria();
+                        break;
+                    case 3:
                         ListarContas();
                         break;
                     case 0:
@@ -48,7 +52,7 @@ namespace Lc.Csharp.Modulo1.Praticando
             } while (run != 0);
         }
 
-        static void AdicionarConta()
+        static void AdicionarContaUniversitaria()
         {
             Console.WriteLine("Digite um nome:");
             string nome = Console.ReadLine();
@@ -64,7 +68,44 @@ namespace Lc.Csharp.Modulo1.Praticando
             DateTime modificado = DateTime.Now;
             double saldo = 1;
 
-            ContaBancaria contaBancaria = new ContaBancaria(id, nome, cpf, email, saldo, criado, modificado);
+            ContaMax contaBancaria = new ContaMax(id, nome, cpf, email, saldo, criado, modificado);
+
+            bool sucesso = false;
+            for (int i = 0; i < contas.Length; i++)
+            {
+                if (contas[i] != null)
+                    continue;
+                else
+                {
+                    contas[i] = contaBancaria;
+                    sucesso = true;
+                    break;
+                }
+            }
+
+            if (sucesso)
+                Console.WriteLine("Adicionado com sucesso!");
+            else
+                Console.WriteLine("Não foi possivel adicionar. Lista cheia!");
+        }
+
+        static void AdicionarContaMax()
+        {
+            Console.WriteLine("Digite um nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o CPF:");
+            int cpf = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um email:");
+            string email = Console.ReadLine();
+
+            string id = new Guid().ToString();
+            DateTime criado = DateTime.Now;
+            DateTime modificado = DateTime.Now;
+            double saldo = 1;
+
+            ContaMax contaBancaria = new ContaMax(id, nome, cpf, email, saldo, criado, modificado);
 
             bool sucesso = false;
             for (int i = 0; i < contas.Length; i++)
@@ -91,14 +132,25 @@ namespace Lc.Csharp.Modulo1.Praticando
             foreach (var conta in contas)
             {
                 if (conta != null)
-                    conta.ImprimirConta();
+                {
+                    if (conta.GetType().Name == typeof(ContaUniversitaria).Name)
+                    {
+                        ContaUniversitaria contaU = (ContaUniversitaria)conta;
+                        contaU.ImprimirConta();
+                    }
+                    else
+                    {
+                        ContaMax contaM = (ContaMax)conta;
+                        contaM.ImprimirConta();
+                    }
+                }
             }
         }
     }
 
-    class ContaBancaria
+    class Conta
     {
-        private double saldo;
+        public double saldo;
         public readonly string Id;
 
         public string Nome { get; set; }
@@ -119,7 +171,7 @@ namespace Lc.Csharp.Modulo1.Praticando
         public DateTime Modificado { get; set; }
         public DateTime Criado { get; }
 
-        public ContaBancaria(string id, string n, int c, string e, double s, DateTime cr, DateTime m)
+        public Conta(string id, string n, int c, string e, double s, DateTime cr, DateTime m)
         {
             Id = id;
             Nome = n;
@@ -129,26 +181,51 @@ namespace Lc.Csharp.Modulo1.Praticando
             Criado = cr;
             Modificado = m;
         }
+    }
+
+    class ContaUniversitaria : Conta
+    {
+        public static double Taxa = 1.2;
+
+        public ContaUniversitaria(string id, string n, int c, string e, double s, DateTime cr, DateTime m)
+           : base(id, n, c, e, s, cr, m)
+        {
+        }
 
         public void ImprimirConta()
         {
+            Console.WriteLine("\nConta Universitária");
             Console.WriteLine($"\nIF: {Id} \nNome: {Nome} \nCPF: {Cpf} \nE-mail: {Email} \nSaldo: {saldo.ToString("C")} \nCriado: {Criado} \nModificado: {Modificado}");
+        }
+    }
+
+    class ContaMax : Conta
+    {
+        public static double Taxa = 1.2;
+
+        public ContaMax(string id, string n, int c, string e, double s, DateTime cr, DateTime m)
+           : base(id, n, c, e, s, cr, m)
+        {
+        }
+
+        public void ImprimirConta()
+        {
+            Console.WriteLine("\nConta Max");
+            Console.WriteLine($"\nID: {Id} \nNome: {Nome} \nCPF: {Cpf} \nE-mail: {Email} \nSaldo: {saldo.ToString("C")} \nCriado: {Criado} \nModificado: {Modificado}");
         }
     }
 
     static class Investimento
     {
-        public const double alicota = 1.1;
-
-        public static double InvestimentoFixo(int prazo, double valor)
+        public static double InvestimentoFixo(int taxa, double valor)
         {
-            return valor * prazo * alicota;
+            return valor * taxa;
         }
 
-        public static double InvestimentoVariavel(int prazo, double valor)
+        public static double InvestimentoVariavel(int taxa, double valor)
         {
-            double taxa = new Random(1).NextDouble();
-            return valor * prazo * taxa;
+            double taxaVariable = new Random(1).NextDouble();
+            return valor * taxa * taxaVariable;
         }
     }
 }
