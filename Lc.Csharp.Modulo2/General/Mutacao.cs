d0f628b82256f99be41144463e912daf4bc79579
Tiago Eduardo <tiago.eduardo@capgemini.com>
2020-03-18 20:29:26 -0300

    Update interfaces.

---
+++ Lc.Csharp.Modulo2/General/Mutacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lc.Csharp.Modulo2.General
+{
+    class Mutacao : INadar, IVoar
+    {
+        public string BaterAsas()
+        {
+            return "Bater asas!";
+        }
+
+        public string MexerNadadeiras()
+        {
+            return "Nadando!";
+        }
+    }
+}
---
+++ Lc.Csharp.Modulo2/General/Passaro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lc.Csharp.Modulo2.General
+{
+    class Passaro : IVoar
+    {
+        public string BaterAsas()
+        {
+            return "Bater asas!";
+        }
+    }
+}
