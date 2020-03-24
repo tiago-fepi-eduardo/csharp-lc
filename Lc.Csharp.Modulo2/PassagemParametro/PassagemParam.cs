aa32e02bc6018bcc6b826a87bef48ee859fee9f5
Tiago Eduardo <tiago.eduardo@capgemini.com>
2020-03-23 21:09:42 -0300

    Update params.

--- Lc.Csharp.Modulo2/PassagemParametro/PassagemParam.cs
+++ Lc.Csharp.Modulo2/PassagemParametro/PassagemParam.cs
@@ -6,27 +6,27 @@ namespace Lc.Csharp.Modulo2.PassagemParametro
 {
     class PassagemParam
     {
-        static void Main()
-        {
-            // Passagem de parametro padrao
-            int x = NormalFunc(100);
-            Console.WriteLine(x);
+        //static void Main()
+        //{
+        //    // Passagem de parametro padrao
+        //    int x = NormalFunc(100);
+        //    Console.WriteLine(x);
 
-            // Passagem por referencia
-            int y = 200;
-            RefFunc(ref y);
-            Console.WriteLine(y);
+        //    // Passagem por referencia
+        //    int y = 200;
+        //    RefFunc(ref y);
+        //    Console.WriteLine(y);
 
-            // Funcao que retorna out
-            int z = 300;
-            OutFunc(z, out int zz);
-            Console.WriteLine(zz);
+        //    // Funcao que retorna out
+        //    int z = 300;
+        //    OutFunc(z, out int zz);
+        //    Console.WriteLine(zz);
 
-            // Passagem de uma lista de valores diferente
-            int w = 400;
-            string s = "Propriedade";
-            VarFunc(s, w);
-        }
+        //    // Passagem de uma lista de valores diferente
+        //    int w = 400;
+        //    string s = "Propriedade";
+        //    VarFunc(s, w);
+        //}
 
         static int NormalFunc(int num)
         {
---
+++ Lc.Csharp.Modulo2/PassagemParametro/PraticaParam.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lc.Csharp.Modulo2.PassagemParametro
+{
+    class PraticaParam
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Entre com o primeiro valor: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o segundo valor: ");
+            int b = int.Parse(Console.ReadLine());
+
+            // Soma
+            int soma = NormalFuncSoma(a, b);
+            Console.WriteLine("Soma = " + soma);
+
+            // Subtracao
+            int sub = 0;
+            RefFuncSubtracao(a, b, ref sub);
+            Console.WriteLine("Subtracao = " + sub);
+
+            // Multiplicacao
+            OutFuncMultiplicacao(a, b, out int mult);
+            Console.WriteLine("Multiplicacao = " + mult);
+
+            // Divisao
+            VarFuncDivisao(a, b);
+        }
+
+        static int NormalFuncSoma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void RefFuncSubtracao(int a, int b, ref int sub)
+        {
+            sub = a - b;
+        }
+
+        static void OutFuncMultiplicacao(int a, int b, out int res)
+        {
+            res = a * b;
+        }
+
+        static void VarFuncDivisao(params object[] extraParams)
+        {
+            int result = int.Parse(extraParams[0].ToString()) / int.Parse(extraParams[1].ToString());
+
+            Console.WriteLine("Divisao = " + result);
+        }
+    }
+}
